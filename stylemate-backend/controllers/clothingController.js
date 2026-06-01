81b7c1e282c20ff2a22f43d559e8e9f22b4a0110
Khyati Anand <khyatianand1134@gmail.com>
2026-06-02 01:52:27 +0530

    Complete clothing item CRUD operations

--- stylemate-backend/controllers/clothingController.js
+++ stylemate-backend/controllers/clothingController.js
@@ -28,4 +28,77 @@ const getMyWardrobe = async (req, res) => {
     wardrobe,
   });
 };
-module.exports = { addClothingItem, getMyWardrobe };
+
+const deleteClothingItem = async (req, res) => {
+  const clothingId = req.params.id;
+  const userId = req.user.id;
+
+  const clothingItem = await ClothingItem.findOne({
+    where: { id: clothingId },
+  });
+
+  if (!clothingItem) {
+    return res.status(404).json({
+      message: "clothing item did not found",
+    });
+  }
+
+  if (clothingItem.userId !== userId) {
+    return res.status(403).json({
+      message: "not authorized to delete this item",
+    });
+  }
+
+  await clothingItem.destroy();
+  return res.status(200).json({
+    message: "deleted successfully",
+  });
+};
+
+const updateClothingItem = async (req, res) => {
+  const clothingId = req.params.id;
+  const userId = req.user.id;
+  const { name, category, color, style, imageUrl } = req.body;
+  const clothingItem = await ClothingItem.findOne({
+    where: { id: clothingId },
+  });
+
+  if (!clothingItem) {
+    return res.status(404).json({
+      message: "item not found",
+    });
+  }
+
+  if (clothingItem.userId !== userId) {
+    return res.status(403).json({
+      message: "not authorized to update",
+    });
+  }
+  if (name) {
+    clothingItem.name = name;
+  }
+  if (category) {
+    clothingItem.category = category;
+  }
+  if (color) {
+    clothingItem.color = color;
+  }
+  if (style) {
+    clothingItem.style = style;
+  }
+  if (imageUrl) {
+    clothingItem.imageUrl = imageUrl;
+  }
+
+  await clothingItem.save();
+  res.status(200).json({
+    message: "updated successfully",
+    clothingItem,
+  });
+};
+module.exports = {
+  addClothingItem,
+  getMyWardrobe,
+  deleteClothingItem,
+  updateClothingItem,
+};
--- stylemate-backend/routes/clothingRoutes.js
+++ stylemate-backend/routes/clothingRoutes.js
@@ -3,8 +3,12 @@ const authMiddleware = require("../middleware/authMiddleware");
 const {
   addClothingItem,
   getMyWardrobe,
+  deleteClothingItem,
+  updateClothingItem,
 } = require("../controllers/clothingController");
 const router = express.Router();
 router.post("/addClothes", authMiddleware, addClothingItem);
 router.get("/my-wardrobe", authMiddleware, getMyWardrobe);
+router.delete("/:id", authMiddleware, deleteClothingItem);
+router.put("/:id", authMiddleware, updateClothingItem);
 module.exports = router;
